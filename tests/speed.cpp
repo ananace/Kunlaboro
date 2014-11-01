5ce1a6033ada573ebb6f61b849ca05604d1dc774
Alexander "Ace" Olofsson <ace@haxalot.com>
2014-11-01 18:43:22 +0100

    Add a speed benchmark

---
+++ tests/speed.cpp
@@ -0,0 +1,34 @@
+#include <Kunlaboro/Component.hpp>
+#include <Kunlaboro/EntitySystem.hpp>
+#include "catch.hpp"
+
+SCENARIO("Message passing benchmark",
+    "[benchmark]")
+{
+    class BasicComponent : public Kunlaboro::Component
+    {
+    public:
+        BasicComponent() : Kunlaboro::Component("Basic") { }
+        void addedToEntity() {
+            requestMessage("Ping", [this](const Kunlaboro::Message& msg) {
+                sendGlobalMessage("Pong");
+            });
+        }
+    };
+
+    Kunlaboro::EntitySystem es;
+
+    es.registerComponent<BasicComponent>("Basic");
+    es.registerTemplate("Basic", { "Basic" });
+
+    std::vector<Kunlaboro::EntityId> eids = {
+        es.createEntity("Basic"),
+        es.createEntity("Basic"),
+        es.createEntity("Basic"),
+        es.createEntity("Basic")
+    };
+
+    Kunlaboro::Message msg;
+    for (uint32_t i = 0; i < 1000000; ++i)
+        es.sendGlobalMessage(es.getMessageRequestId("Ping"), msg);
+}
