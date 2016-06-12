cb2870d36c34598ec1d121a57247607338ab3741
Alexander "Ace" Olofsson <ace@haxalot.com>
2016-06-12 22:50:07 +0200

    Added Kunlaboro.hpp file

---
+++ include/Kunlaboro/Kunlaboro.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Component.inl"
+#include "Entity.inl"
+#include "EntitySystem.inl"
+#include "Event.inl"
+#include "EventSystem.inl"
+#include "Message.inl"
+#include "MessageSystem.inl"
+#include "Views.inl"
